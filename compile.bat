d24c9d56d606174f57c8fbf9af2304d74030e3bf
Raul Anchorena <raul.anchorena@geosystems.com.ar>
2024-03-02 14:28:54 -0300

    Ajuste en archivo compile.bat

--- compile.bat
+++ compile.bat
@@ -4,4 +4,4 @@ call "C:\Program Files\QGIS 3.16\bin\qt5_env.bat"
 call "C:\Program Files\QGIS 3.16\bin\py3_env.bat"
 
 @echo on
-"C:\Program Files\QGIS 3.16\apps\Python37\Scripts\pyrcc5" -o "C:\Users\Admin\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\rancho_tools_plugin\resources.py" "C:\Users\Admin\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\rancho_tools_plugin\resources.qrc"
+"C:\Program Files\QGIS 3.16\apps\Python37\Scripts\pyrcc5" -o "C:\Users\ranch\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\rancho_tools_plugin\resources.py" "C:\Users\ranch\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\rancho_tools_plugin\resources.qrc"
